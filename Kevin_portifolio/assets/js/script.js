69ca343324daf380d699cc18ed9e23ed742fa045
kevin lopes albuquerque <kevinup13@gmail.com>
2022-10-11 19:06:28 -0400

    inserindo section

--- Kevin_portifolio/assets/js/script.js
+++ Kevin_portifolio/assets/js/script.js
@@ -6,7 +6,7 @@ function subirTela() {
 };
 
 function btnScroll() {
-    if (window.scrollY === 0 || window.scrollY > 2175) {
+    if (window.scrollY === 0 || window.scrollY > 2860) {
         //ocutar o btn
         document.querySelector('.scrollbutton').style.display = 'none';
     } else {
